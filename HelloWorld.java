b3301e145311a4a80256537dbd3b5f6fb0fff968
pragya234 <pandeypragya026@gmail.com>
2020-10-27 00:53:55 +0530

    I have staged two files for commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 public static void main(String args[]){
 System.out.println("Hello world");
 System.out.println("This is a single file application");
+System.out.println("I am doing changes after staging the file");
 }
 }
---
+++ IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse{
+public static void main(String args[]){
+int x=7;
+if(x<10){
+System.out.println("x is less than 10");}
+
+else{
+System.out.println("x is more than 10");
+}
+}
+}
